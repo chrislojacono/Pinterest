1412cfce622afca222d1661a9015252a6c95053e
Chris LoJacono <clojacono92@gmail.com>
2020-09-24 11:59:19 -0500

    got boards to appear and styled a bit

--- src/javascripts/components/cards/boardMaker.js
+++ src/javascripts/components/cards/boardMaker.js
@@ -1,8 +1,8 @@
 const boardMaker = (board) => {
   const domString = `<div class="card" id="${board.uid}">
-  <div class="img-container card-body" style="background-image: url(${board.image})">
-      <h5 class="card-title">${board.name}</h5>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="img-container card-body" style="background-image:linear-gradient(to bottom, rgba(209, 209, 219, 0.253), rgba(204, 174, 195, 0.73)), url(${board.image})">
+      <h3 class="card-title">${board.name}</h3>
+      <a href="#" id="board-button" class="btn btn-info">Pins</a>
     </div>
   </div>`;
   return domString;
