fb26a3b6ad3543159b6e9fc776afb9ccdfb445cd
Chris LoJacono <clojacono92@gmail.com>
2020-10-05 11:52:51 -0500

    styling and clickable images

--- src/javascripts/components/cards/boardMaker.js
+++ src/javascripts/components/cards/boardMaker.js
@@ -5,8 +5,10 @@ const boardMaker = (board) => {
   const domString = `<div class="card" id="${board.uid}">
   <div class="img-container card-body" style="background-image:linear-gradient(to bottom, rgba(209, 209, 219, 0.253), rgba(204, 174, 195, 0.73)), url(${board.image})">
       <h3 class="card-title">${board.name}</h3>
-      <a href="#" id="${board.uid}" class="btn btn-info see-pins">Pins</a>
-      <button id="${board.uid}" class="btn btn-danger delete-board"> Delete Board </button>
+    </div>
+    <div class="board-button">
+    <a href="#" id="${board.uid}" class="btn btn-info see-pins board-buttons"><i id="pin-icon" class="fas fa-map-pin"></i> Pins</a>
+    <button id="${board.uid}" class="btn btn-danger delete-board board-buttons"><i id="pin-icon" class="far fa-trash-alt"></i> Delete Board </button>
     </div>
   </div>`;
   $('body').on('click', '.delete-board', (e) => {
